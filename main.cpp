0640d026d0b4032355aaf716447dbc5abf3aaca6
dimanchique <ghettorunner@yandex.ru>
2022-12-08 09:34:07 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,9 +2,9 @@
 #include "Vector3D.h"
 
 int main() {
-    const auto vec1 = Vector3D(3,-2,5);
-    const auto vec2 = Vector3D(1,-1, 3);
-    const auto vec3 = Vector3D(-2,2, 1);
+    const auto vec1 = Vector3D(3, -2, 5);
+    const auto vec2 = Vector3D(1, -1, 3);
+    const auto vec3 = Vector3D(-2, 2, 1);
     printf("Triple product: %.4f\n", Vector3D::GetTripleProduct(vec1, vec2, vec3));
     return 0;
 }
